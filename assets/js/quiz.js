08e7d8b4770ad89b6c157a578f89fa8ba0b9dc9a
JoyZadan <joyzadan.learning@gmail.com>
2022-03-02 00:50:10 +0000

    Added media queries for the quiz game page

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,26 @@
 // setting up the quiz game structure
 const startQuiz = document.querySelector(".start-quiz");
+const mapLink = document.querySelector(".map-link");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mapLink.addEventListener("click", openMap => {
+    window.location.href = "find.html";
+})

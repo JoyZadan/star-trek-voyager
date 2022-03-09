db6ce9d15ee65a4ccace4157237f7735498babc0
JoyZadan <joyzadan.learning@gmail.com>
2022-03-09 02:15:58 +0000

    Adjusted media query on games links on index.html for mid-sized screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,39 @@
 // Setting up the event handlers 
-const quizLink = document.getElementsByClassName(".quiz-link");
-const quotesLink = document.getElementsByClassName(".quotes-link");
-const rescueLink = document.getElementsByClassName(".rescue-link");
+// const quizLink = document.getElementsByClassName(".quiz-link");
+// const quotesLink = document.getElementsByClassName(".quotes-link");
+// const rescueLink = document.getElementsByClassName(".rescue-link");
 const mapLink = document.getElementsByClassName(".map-link");
 
 // This changes the background color of the quiz link when clicked
-quizLink.addEventListener("click", changeBackground => {
-    quizLink.style.backgroundColor = "#efb200";
+// quizLink.addEventListener("click", changeBackground => {
+//     quizLink.style.backgroundColor = "#efb200";
 
-    // This reverts the background color of the quiz link after 3 seconds
-    window.setTimeout(() => {
-        quizLink.style.backgroundColor = "#29407c";
-    }, 3000); 
-});
+//     // This reverts the background color of the quiz link after 3 seconds
+//     window.setTimeout(() => {
+//         quizLink.style.backgroundColor = "#29407c";
+//     }, 3000); 
+// });
 
 // This changes the background color of the quotes link when clicked
-quotesLink.addEventListener("click", () => {
-    quotesLink.style.backgroundColor = "#efb200";
+// quotesLink.addEventListener("click", () => {
+//     quotesLink.style.backgroundColor = "#efb200";
 
-    // This reverts the background color of the quiz link after 3 seconds
-    window.setTimeout(() => {
-        quotesLink.style.backgroundColor = "#600";
-    }, 3000); 
-});
+//     // This reverts the background color of the quiz link after 3 seconds
+//     window.setTimeout(() => {
+//         quotesLink.style.backgroundColor = "#600";
+//     }, 3000); 
+// });
 
 // This changes the background color of the rescue mission link when clicked
-rescueLink.addEventListener("click", changeBackground => {
-    rescueLink.style.backgroundColor = "#efb200";
+// rescueLink.addEventListener("click", changeBackground => {
+//     rescueLink.style.backgroundColor = "#efb200";
 
-    // This reverts the background color of the rescue mission link after 3 seconds
-    window.setTimeout(() => {
-        rescueLink.style.backgroundColor = "#efb200";
-    }, 3000); 
-});
+//     // This reverts the background color of the rescue mission link after 3 seconds
+//     window.setTimeout(() => {
+//         rescueLink.style.backgroundColor = "#efb200";
+//     }, 3000); 
+// });
 
-mapLink.addEventListener("click", openMap => {
+mapLink.addEventListener("click", (openMap) => {
     window.location.href = "find.html";
 });
